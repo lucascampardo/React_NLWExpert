5973a47b54e4af0e834a46eb1f84d44a410347f6
lucas <lucascampardo@outlook.com>
2024-02-10 10:12:06 -0300

    Atualização final

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -2,23 +2,19 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { X } from "lucide-react";
-import { FormEvent } from "react";
 import { toast } from "sonner";
 
 interface NoteCardProps {
   note: {
+    id: string;
     date: Date;
     content: string;
   };
+
+  onNoteDeleted: (id: string) => void;
 }
 
-function handleDeleteNote(event: FormEvent) {
-  event.preventDefault();
-
-  toast.success("Nota excluída com sucesso!");
-}
-
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-line-400">
@@ -33,12 +29,12 @@ export function NoteCard({ note }: NoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50"></Dialog.Overlay>
-        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] md:h-[60vh] w-full bg-slate-700 md:rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
 
-          <form onSubmit={handleDeleteNote} className="flex-1 flex flex-col">
+          <form className="flex-1 flex flex-col">
             <div className="flex flex-1 flex-col gap-3 p-5">
               <span className="text-sm font-medium text-slate-200">
                 {" "}
@@ -53,6 +49,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
             <button
               type="submit"
+              onClick={() => onNoteDeleted(note.id)}
               className="w-full bg-red-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group hover:bg-red-700"
             >
               Deseja{" "}
